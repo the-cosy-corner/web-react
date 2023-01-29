9ebf2d83604c7150071693e704d4ed06433ee190
Ian Parr <ianjudparr@gmail.com>
2023-01-29 18:23:11 +0000

    QR Code

--- src/pages/EvertonBrow.js
+++ src/pages/EvertonBrow.js
@@ -1,8 +1,48 @@
 import React, { Component } from "react";
+import { Container, Accordion } from "react-bootstrap";
+import QRCode from "react-qr-code";
 
 export default class EvertonBrow extends Component {
   state = {};
   render() {
-    return <div className="App">Everton Brow</div>;
+    return (
+      <>
+        <Container>
+          
+          <h4>Guest Information</h4>
+          <Accordion>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Keys</Accordion.Header>
+              <Accordion.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Underground Parking</Accordion.Header>
+              <Accordion.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="2">
+              <Accordion.Header>Electronics</Accordion.Header>
+              <Accordion.Body>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+<br/>
+          <div style={{ height: "auto", margin: "0 auto", maxWidth: 64, width: "100%" }}>
+            <QRCode size={256} style={{ height: "auto", maxWidth: "100%", width: "100%" }} value={'https://staging--the-cosy-corner-dev.netlify.app/everton-brow'} viewBox={`0 0 256 256`} />
+          </div>
+
+        </Container>
+      </>
+    );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, Container, Button } from "react-bootstrap";
+import { Card, Container, Button, Nav } from "react-bootstrap";
 import evertonbrow_title from "../images/properties/everton-brow/title.jpg";
 
 export default class Home extends Component {
@@ -16,7 +16,7 @@ export default class Home extends Component {
               With its close proximity to a range of shops, restaurants, and cultural attractions, you'll never run out of things to do. The townhouse itself boasts spacious rooms, modern amenities,
               and a cozy atmosphere that makes it the ideal home away from home. Whether you're traveling for business or leisure, this rental is the perfect choice for your stay.
             </Card.Text>
-            <Button variant="primary">View Property</Button>
+            <Nav.Link href="/everton-brow" className="btn btn-primary">View Property</Nav.Link>
           </Card.Body>
         </Card>
       </Container>
