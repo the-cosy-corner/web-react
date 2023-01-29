ec02cf1efe27e9eabb33789a78995a16c9556022
Ian Parr <ianjudparr@gmail.com>
2023-01-29 09:21:18 +0000

    Add react router. Change main menu

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -14,7 +14,7 @@ export class MainMenu extends Component {
     return (
         <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">The Cosy Corner</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
