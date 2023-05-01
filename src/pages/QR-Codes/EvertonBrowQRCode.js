7a28b735ebe40dd9217a7a945fad1b38c9066e23
Ian Parr <ianjudparr@gmail.com>
2023-05-01 19:35:55 +0100

    Make QR code smaller

--- src/pages/QR-Codes/EvertonBrowQRCode.js
+++ src/pages/QR-Codes/EvertonBrowQRCode.js
@@ -5,11 +5,13 @@ export default class EvertonBrowQrCode extends Component {
   state = {};
   render() {
     return (
-      <>
-        <div style={{ height: "auto", margin: "0 auto", maxWidth: 512, width: "100%" }}>
+      <div style={{ "padding-bottom": "2rem", textAlign: "center" }}>
+        <h4>House Manuals & Information</h4>
+        <hr/>
+        <div style={{ height: "auto", margin: "0 auto", maxWidth: 350, width: "100%" }}>
           <QRCode size={256} style={{ height: "auto", maxWidth: "100%", width: "100%" }} value={"https://the-cosy-corner.netlify.app/everton-brow"} viewBox={`0 0 256 256`} />
         </div>
-      </>
+      </div>
     );
   }
 }
